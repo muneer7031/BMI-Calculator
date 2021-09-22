c4922823ad8b503efffb89d56aba91b2275f20bf
muneer <muneer7031@gmail.com>
2021-09-22 20:18:07 +0530

    fixed navigation of drawer bar to new screens.Deleted menu.dart inorder to throw out nesting of routes

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -2,7 +2,7 @@ import 'package:bmi_calc/widgets/gender.dart';
 import 'package:flutter/material.dart';
 import 'package:bmi_calc/widgets/MySlider.dart';
 import 'package:bmi_calc/widgets/agebar.dart';
-import 'package:bmi_calc/screens/menu.dart';
+// import 'package:bmi_calc/screens/menu.dart';q
 // import 'images';
 class Home extends StatelessWidget {
    Home({ Key? key }) : super(key: key);
@@ -16,7 +16,61 @@ class Home extends StatelessWidget {
       
       home: SafeArea(
         child: Scaffold(
-          drawer: menu(),
+          drawer: Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text(
+              '',
+              style: TextStyle(color: Color(0xffF7F7FF), fontSize: 25),
+            ),
+            decoration: BoxDecoration(
+                color: Color(0xff031926),
+                image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage('images/img.png'))),
+          ),
+          ListTile(
+            leading: Icon(Icons.input),
+            title: Text('Check my BMI',
+            style: TextStyle(color: Color(0xff031926),
+             ),
+            
+            ),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.verified_user),
+            title: Text('What is BMI Calculator?',
+            style: TextStyle(color: Color(0xff031926),
+             ),),
+            onTap: ()  {Navigator.pushNamed(context, '/info');},
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings',
+            style: TextStyle(color: Color(0xff031926),
+             ),),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.border_color),
+            title: Text('Feedback',
+            style: TextStyle(color: Color(0xff031926),
+             ),),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout',
+            style: TextStyle(color: Color(0xff031926),
+             ),),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+        ],
+      ),
+    ),
           backgroundColor:Colors.black45, 
           appBar: AppBar(
             backgroundColor: Colors.black45,
@@ -43,7 +97,7 @@ class Home extends StatelessWidget {
               Container(
                 child: age()
                 ),
-              SizedBox(height: 100,),
+              SizedBox(height: 70,),
 
               ElevatedButton(
                 style: raisedButtonStyle,
