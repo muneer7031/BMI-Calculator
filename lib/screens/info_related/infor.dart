0855a39f917aa40f27c46f2b865e61204dd23677
muneer <muneer7031@gmail.com>
2021-09-23 18:22:00 +0530

    added proper information about 'what is bmi calulator' and made proper fonts with padding

--- lib/screens/info_related/infor.dart
+++ lib/screens/info_related/infor.dart
@@ -19,13 +19,23 @@ class info extends StatelessWidget {
                                     )
                                   )),
           ),
-          body: Container(
-            
+          body: Padding(
+            padding:EdgeInsets.only(left:10.0,top: 5,right: 10),
             child: SingleChildScrollView(
               scrollDirection: Axis.vertical,
               child: RichText(
                 text: TextSpan(
                   children: <TextSpan>[
+                    TextSpan(
+                      text: 'What is BMI \n',
+                      spellOut: true,
+                      style: GoogleFonts.architectsDaughter(
+                        textStyle: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold
+                        )
+                      )
+                    ),
                     TextSpan(
                       text: 'BMI ',
                       spellOut: true,
@@ -44,12 +54,28 @@ class info extends StatelessWidget {
                         letterSpacing: 1,
                         fontSize: 20
                       )
-                      ) 
-                      
-                      
-                      
+                      )
                     ),
-                    
+                    TextSpan(
+                      text: '\nBMI Calculator: ',
+                      spellOut: true,
+                      style: GoogleFonts.architectsDaughter(
+                        textStyle: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold
+                        )
+                      )
+                    ),
+                    TextSpan(
+                      text: "\n A BMI calculator is an online or offline device which measures your body mass index. The body mass index or BMI is a measure of how much body mass you have in relation to your height and weight. \nBody mass refers not only to the fat within your body but also within muscles and bones. It is calculated by taking your weight and dividing it by the square of your height. The BMI figures let you know whether you are within normal weight range, or if you are underweight or overweight.\nBoth high BMIs and low ones are known to raise a host of health problems, especially as you age. However, a high BMI in itself is not always, medically speaking, a cause for alarm. It should be viewed in conjunction with other health parameters.",
+                      spellOut: true,
+                      style: GoogleFonts.shadowsIntoLight(
+                        textStyle: TextStyle(
+                          fontSize: 20,
+                          // fontWeight: FontWeight.bold
+                        )
+                      )
+                    ),
                   ]
                 ),
               ),
